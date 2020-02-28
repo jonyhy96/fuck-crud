1b5ac7886834887f263acaaece805e450ccfe899
jonyhy96 <hy352144278@gmail.com>
2020-02-28 22:51:23 +0800

    feat: support normal json tag

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/jonyhy96/fuck-crud
 go 1.13
 
 require (
+	github.com/iancoleman/strcase v0.0.0-20191112232945-16388991a334
 	github.com/spf13/cobra v0.0.6
 	github.com/spf13/viper v1.6.2
 	github.com/stretchr/testify v1.4.0 // indirect
--- pkg/util/template/template.go
+++ pkg/util/template/template.go
@@ -1,8 +1,11 @@
 package template
 
 import (
+	"fmt"
 	"html/template"
 	"strings"
+
+	"github.com/iancoleman/strcase"
 )
 
 // GetTemplate gets a template.
@@ -10,6 +13,15 @@ func GetTemplate() *template.Template {
 	return template.New("t").Funcs(template.FuncMap{
 		"upperFirst": func(origin string) string {
 			return strings.Title(strings.ToLower(origin))
+		},
+		"jsontag": func(origin string) string {
+			return fmt.Sprintf("`json:\"%s\"`", strcase.ToLowerCamel(origin))
+		},
+		"lowerCamel": func(origin string) string {
+			return strcase.ToLowerCamel(origin)
+		},
+		"camel": func(origin string) string {
+			return strcase.ToCamel(origin)
 		},
 	})
 }
